0f18444dd628090d10f998c007ffcb47867af3d7
Mike Daugherty <mikedaugherty@msdwebdesigns.com>
2018-03-30 09:35:32 -0600

    Init Commit

---
+++ src/js/components/SelectCurrency.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const SelectCurrency = (props) => {
+    const {currencies, onSelectCurrency} = props;
+    return <select onChange={(e) => onSelectCurrency(e.target.value)}>
+        {
+            currencies.map(currency => {
+                const{code, name} = currency;
+                return <option key={code} value={code}>{name}</option>
+            })
+        }
+    </select>
+}
+
+
+SelectCurrency.propTypes = {
+    currencies: PropTypes.array.isRequired,
+    onSelectCurrency: PropTypes.func.isRequired
+}
+export default SelectCurrency;
